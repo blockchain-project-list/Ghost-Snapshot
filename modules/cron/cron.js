ed3bdce01f1acd833703265b9b3a6b2313b6c30d
buddana98 <avinash.buddana@minddeft.com>
2021-07-02 15:37:37 +0530

    cron disabled

--- modules/cron/cron.js
+++ modules/cron/cron.js
@@ -2,9 +2,9 @@ const cron = require('node-cron');
 const DailyCron = require('./getDailyData');
 const BlockPassCtr = require('../../modules/blockpass/blockpassCtr');
 
-cron.schedule('0 */24 * * *', (req, res) => {
-  DailyCron.getContractsData(req, res);
-});
+// cron.schedule('0 */24 * * *', (req, res) => {
+//   DailyCron.getContractsData(req, res);
+// });
 
 cron.schedule('0 22 * * *', (req, res) => {
   BlockPassCtr.getApprovedUserList(req, res);
