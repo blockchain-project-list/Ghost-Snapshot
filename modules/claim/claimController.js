af38a37413895409d798235880ac46666805653a
buddana98 <avinash.buddana@minddeft.com>
2021-09-04 16:29:15 +0530

    calim changes

--- modules/claim/claimController.js
+++ modules/claim/claimController.js
@@ -15,24 +15,42 @@ ClaimCtr.addNewClaim = async (req, res) => {
       phaseNo,
       logo,
     } = req.body;
-    const addNewClaim = new ClaimModel({
-      tokenAddress: tokenAddress,
-      networkName: networkName,
-      networkSymbol: networkSymbol,
-      networkId: networkId,
-      amount: amount,
-      name: name,
-      timestamp,
-      phaseNo,
-      logo,
+
+    const checkClaimAlreadyAdded = await ClaimModel.findOne({
+      phaseNo: phaseNo,
+      tokenAddress: tokenAddress.toLowerCase(),
+      networkSymbol: networkSymbol.toUpperCase(),
     });
 
-    await addNewClaim.save();
+    if (checkClaimAlreadyAdded) {
+      checkClaimAlreadyAdded.amount += +amount;
 
-    return res.status(200).json({
-      message: 'Claim Added sucessfully',
-      status: true,
-    });
+      await checkClaimAlreadyAdded.save();
+
+      return res.status(200).json({
+        message: 'Claim Added sucessfully',
+        status: true,
+      });
+    } else {
+      const addNewClaim = new ClaimModel({
+        tokenAddress: tokenAddress,
+        networkName: networkName,
+        networkSymbol: networkSymbol,
+        networkId: networkId,
+        amount: amount,
+        name: name,
+        timestamp,
+        phaseNo,
+        logo,
+      });
+
+      await addNewClaim.save();
+
+      return res.status(200).json({
+        message: 'Claim Added sucessfully',
+        status: true,
+      });
+    }
   } catch (err) {
     return res.status(500).json({
       message: 'DB_ERROR',
