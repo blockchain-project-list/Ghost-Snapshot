cbdd7842f7b6d60400f6d61be4464fdafa42df4e
buddana98 <avinash.buddana@minddeft.com>
2021-10-15 15:44:26 +0530

    cron changes

--- modules/cron/cron.js
+++ modules/cron/cron.js
@@ -7,9 +7,9 @@ const BlockPassCtr = require('../../modules/blockpass/blockpassCtr');
 //   DailyCron.getContractsData(req, res);
 // });
 
-cron.schedule('0 */12 * * *', (req, res) => {
-  BlockPassCtr.getApprovedUserList(req, res);
-});
+// cron.schedule('0 */12 * * *', (req, res) => {
+//   BlockPassCtr.getApprovedUserList(req, res);
+// });
 
 // cron.schedule('0 */12 * * *', (req, res) => {
 //   UserCtr.getUserBalances(req, res);
