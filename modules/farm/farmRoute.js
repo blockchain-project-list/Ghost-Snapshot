3aab42c3ba85a4002304ce7d0286414e67247389
buddana98 <avinash.buddana@minddeft.com>
2021-11-03 17:04:29 +0530

    farm helper added

--- modules/farm/farmRoute.js
+++ modules/farm/farmRoute.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const farmCtr = require('./farmCtr');
+const farmHelper = require('./farmHelper');
 const Auth = require('../../helper/auth');
 
 const farmRoute = express.Router();
@@ -8,4 +9,7 @@ const farmRoute = express.Router();
 const getFarmReport = [farmCtr.getUserBalances];
 farmRoute.get('/getList', getFarmReport);
 
+// farm helper
+const farmHelperRoute = [farmHelper.checkForDuplicate];
+farmRoute.get('/farmList', farmHelperRoute);
 module.exports = farmRoute;
