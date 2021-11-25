82a1499af575aea154b8833255808dea31038165
buddana98 <avinash.buddana@minddeft.com>
2021-11-25 12:18:29 +0530

    console removed

--- config/app.js
+++ config/app.js
@@ -36,21 +36,11 @@ app.use(cors());
 
 app.all('/*', (req, res, next) => {
   let origin = req.headers['origin'];
-
-  console.log('req.headers', req.headers['origin']);
-
-  // if (corsOptions.origin.indexOf(origin) === -1) {
-  //   return res.status(400).json({
-  //     message: 'Unauthrozed',
-  //   });
-  // }
-
   if (corsOptions.origin.indexOf(origin) >= 0) {
-    console.log('IN IF ===>');
     res.header('Access-Control-Allow-Origin', req.headers['origin']);
   } else {
-    return res.status(400).json({
-      message: 'Unauthrozed',
+    return res.status(404).json({
+      message: 'Unauthroized',
     });
   }
   // res.header('Access-Control-Allow-Origin', 'https://snapshot.seedify.fund');
